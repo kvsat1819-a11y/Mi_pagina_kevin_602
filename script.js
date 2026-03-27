ddca6996cc139069b4ae7de550eff3dea8b1f9d1
kvsat1819-a11y <kvsat1819@gmail.com>
2026-03-27 09:56:00 -0600

    l,

---
+++ script.js
@@ -0,0 +1,12 @@
+function mostrarmensaje() {
+    let mensaje=document.getElementById("mensaje");
+    mensaje.innerHTML="cuida y protegea los animales";
+    mensaje.style.display="block";
+}
+    setTimeout(()=> {
+        mensaje.style.opacity="1";
+        mensaje.style.transform="translate y(o)" ;
+    } ,50);
+    
+
+
