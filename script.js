c4a8eeb438a3adaaf510acd0fa44844660e754e6
kvsat1819-a11y <kvsat1819@gmail.com>
2026-04-27 14:10:34 -0600

    a

--- script.js
+++ script.js
@@ -21,29 +21,5 @@ function mostrarmensajepersonal() {
 
 
  
-<script>
-const button = document.getElementById("theme-toggle");
-
-// Cargar preferencia guardada
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark-mode");
-}
-
-button.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-
-  // Guardar preferencia
-  if (document.body.classList.contains("dark-mode")) {
-    localStorage.setItem("theme", "dark");
-  } else {
-    localStorage.setItem("theme", "light");
-  }
-});
-if (!localStorage.getItem("theme")) {
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.body.classList.add("dark-mode");
-  }
-}
-</script>
 
 
